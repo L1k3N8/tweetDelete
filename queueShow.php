1123112775223de685ce021e345dacdedd54578a
L1k3N8 <jones2nt@mail.uc.edu>
2017-11-28 23:49:04 -0500

    cleanup of table

--- queueShow.php
+++ queueShow.php
@@ -1,40 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Twitter Job Runner</title>
-
-
-    <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <script src="./js/jquery.js"></script>
-    <!-- Custom CSS -->
     <link href="css/portfolio-item.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 <link href="./css/clusterize.css" rel="stylesheet">
 <script src="./js/clusterize.min.js"></script>
-
 </head>
-
 <body>
-
-    <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -50,35 +31,18 @@
                     <li>
                         <a href="queueShow.php">Queue</a>
                     </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
                 </ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>
-
-    <!-- Page Content -->
-    <div class="container">
-
-        <!-- Portfolio Item Heading -->
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">Tweet Listing
                 </h1>
             </div>
         </div>
-        <!-- /.row -->
-
-        <!-- Portfolio Item Row -->
         <div class="row" style="height:500px">
              <div class="col-md-8">
-                 words
             <?php 
             global $con;
             include('./include/mysql_con.php');
@@ -86,7 +50,7 @@
             $result = mysqli_query($con, $q);
             if (mysqli_num_rows($result) > 0) {
                 // output data of each row
-                $start = "<table style='border-collapse: separate; border-spacing: 10px;'><thead><tr>Tweet ID</tr><tr>Time Added To Queue</tr></thead><tbody>";
+                $start = "<table style='border-collapse: separate; border-spacing: 10px;'><thead><tr><th>Tweet ID</th><th>Time Added To Queue</th></tr></thead><tbody>";
                 while($row = mysqli_fetch_assoc($result)) {
                     $tim = $row['time_added'];
                     $idno = $row['tweet_id'];
@@ -97,33 +61,9 @@
             }
                 
             ?>
-        
-            </div></div>
-        <!-- /.row -->
-
-        <!-- Related Projects Row -->
-       
-
-        <hr>
-
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
             </div>
-            <!-- /.row -->
-        </footer>
-
-    <!-- /.container -->
-
-    <!-- jQuery -->
+    </div>
     <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
 </body>
-
 </html>
